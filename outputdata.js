6c70c8de81d73248fb6481307b7284bc2e3399d1
Steffen <steffen.haaker@gmail.com>
2017-11-06 20:19:03 +0000

    Branch Merge Andrew

--- outputdata.js
+++ outputdata.js
@@ -20,11 +20,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http); // Socket connection
 var spawn = require('child_process').spawn,
-<<<<<<< HEAD
-    ls    = spawn('python',['trackingwebcam.py']);
-=======
     ls = spawn('python', ['trackingwebcam.py']);
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
 
 // The variables below are for images
 var x;
@@ -83,24 +79,6 @@ var proxRight;
 //Usage: Speed
 var motorLeft = 0;
 var motorRight = 0;
-<<<<<<< HEAD
-
-var encoderLeft = 0;
-var encoderRight = 0;
-
-var mode = 0;
-
-
-var drivestate = 1;
-
-var timeout = 250;
-var action = function(){
-    if(x2 <= 100){
-	robot.driveSpeed(0,50);
-    }
-    else if(x2 >= 380){
-	robot.driveSpeed(50,0);
-=======
 var encoderLeft = 0; //Read the turns in the wheels
 var encoderRight = 0; //Read the turns in the wheels
 var mode = 0; //Control Mode: 1 is Auto -- 2 is Semi-Control -- 3 is Total Control
@@ -113,16 +91,9 @@ var track = function() {
         robot.driveSpeed(50, -50);
     } else {
         robot.driveSpeed(50, 50);
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
     }
     setTimeout(track, timeout)
 }
-<<<<<<< HEAD
-
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/App.html');
-=======
 //End of Variable Declarations
 /***************************************/
 //--------------------------------------
@@ -132,7 +103,6 @@ app.get('/', function(req, res){
 //Var app = require('express')();
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/App.html'); //Sends the App.html file
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
 });
 
 io.on('connection', function(socket) {
@@ -167,26 +137,6 @@ io.on('connection', function(socket) {
 //Emit all of the following on the opened socket
 io.on('connection', function(socket) { // Notify for a new connection and pass the socket as parameter.
     console.log('new connection');
-<<<<<<< HEAD
-    setInterval(function () {
-//        console.log('emit new value', rightBumper);
-				socket.emit('temp', temp); // Emit on the opened socket.
-        socket.emit('rBump', rightBumper); // Emit on the opened socket.
-	socket.emit('lBump', leftBumper); // Emit on the opened socket.
-	socket.emit('lightBumpLeft', lightBumpLeft); // Emit on the opened socket.
-				socket.emit('lightBumpFrontLeft', lightBumpFrontLeft); // Emit on the opened socket.
-				socket.emit('lightBumpCenterLeft', lightBumpCenterLeft); // Emit on the opened socket.
-				socket.emit('lightBumpCenterRight', lightBumpCenterRight); // Emit on the opened socket.
-				socket.emit('lightBumpFrontRight', lightBumpFrontRight); // Emit on the opened socket.
-				socket.emit('lightBumpRight', lightBumpRight); // Emit on the opened socket.
-				socket.emit('charge', charge); // Emit on the opened socket.
-				socket.emit('proxLeft', proxLeft); // Emit on the opened socket.
-				socket.emit('proxFrontLeft', proxFrontLeft); // Emit on the opened socket.
-				socket.emit('proxCenterLeft', proxCenterLeft); // Emit on the opened socket.
-				socket.emit('proxCenterRight', proxCenterRight); // Emit on the opened socket.
-				socket.emit('proxFrontRight', proxFrontRight); // Emit on the opened socket.
-				socket.emit('proxRight', proxRight); // Emit on the opened socket.
-=======
     setInterval(function() {
         socket.emit('temp', temp);
         socket.emit('rBump', rightBumper);
@@ -204,7 +154,6 @@ io.on('connection', function(socket) { // Notify for a new connection and pass t
         socket.emit('proxCenterRight', proxCenterRight);
         socket.emit('proxFrontRight', proxFrontRight);
         socket.emit('proxRight', proxRight);
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
         socket.emit('chargeState', chargeState);
         socket.emit('maxCharge', maxCharge);
         socket.emit('motorLeft', motorLeft);
@@ -216,68 +165,6 @@ io.on('connection', function(socket) { // Notify for a new connection and pass t
         socket.emit('mode', mode);
     }, 100);
 });
-<<<<<<< HEAD
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-
-function updateData(){
-	temp = robot.data.temperature;
-	console.log("Temp:" + robot.data.temperature);
-
-  //console.log("all data test: " + robot.data);
-	//console.log("Charging State:" + robot.data.chargeState);
-
-	// battery data
-    charge = robot.data.charge;
-    if(charge > maxCharge){
-	console.log("Fully charged")
-    }
-    else{
-	console.log("Charging")
-    }
-    
-    //console.log("Current charge:" + robot.data.charge);
-    chargeState = robot.data.chargeState;
-    maxCharge = robot.data.maxCharge;
-    current = robot.data.current;
-    voltage = robot.data.current;
-	// bumper sensors
-	leftBumper = robot.data.bumpLeft;
-	rightBumper = robot.data.bumpRight;
-
-    /// ligth bump true false prox sensors
- 	lightBumpLeft = robot.data.lightBumpLeft;
- 	lightBumpFrontLeft = robot.data.lightBumpFrontLeft;
-	lightBumpCenterLeft = robot.data.lightBumpCenterLeft;
-	lightBumpCenterRight = robot.data.lightBumpCenterRight;
-	lightBumpFrontRight = robot.data.lightBumpFrontRight;
-	lightBumpRight = robot.data.lightBumpRight;
-
-
-	// proximity sensors //0 - 4095
-	proxLeft = robot.data.proxLeft;
-	proxFrontLeft = robot.data.proxFrontLeft ;
-	proxCenterLeft = robot.data.proxCenterLeft ;
-	proxCenterRight = robot.data.proxCenterRight ;
-	proxFrontRight = robot.data.proxFrontRight;
-	proxRight = robot.data.proxRight ;
-
-
-  charger = robot.data.charger;
-  docked = robot.data.docked;
-
-  mode = robot.data.mode;
-
-  encoderLeft = robot.data.encoderLeft;
-  encoderRight = robot.data.encoderRight;
-  console.log(encoderLeft);
-	console.log(encoderRight);
-
-=======
 //var http = require('http').Server(app);
 http.listen(3000, function() {
     console.log('listening on *:3000');
@@ -327,7 +214,6 @@ function updateData() {
     encoderRight = robot.data.encoderRight;
     console.log(encoderLeft);
     console.log(encoderRight);
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
 }
 //End of the Web Output
 //********************************************
@@ -345,148 +231,6 @@ function start() {
 
 //Main Program:
 function main(r) {
-<<<<<<< HEAD
-	robot = r; handleInput(robot);
-
-	//Enter Full Mode:
-	robot.full(); var run = 1;
-
-	//setTimeout(function(){robot.showText("Hello World!", 500, true)}, 500);
-	//We'll play this song whenever entering user-control:
-	robot.setSong(0, [[72,32],[76,32],[79,32],[72,32]]);
-
-	//Handle First onChange:
-	robot.onChange = function() {
-		if(robot.data.charger || robot.data.docked) { robot.start(); run = 0; }
-		else { /*robot.play(0); */driveLogic(); } robot.onChange = onchange;
-	}
-
-	//Handle onChange Events:
-	function onchange(chg) {
-/*	    if(robot.data.mode == 1 && charge >= 2650){
-		robot.data.mode = 3;
-//		robot.drive(-100, -100); drRun = 1;		
-		console.log("undock")
-	    }
-*/
-	    if(robot.data.mode == 3 && run == 1) { //FULL mode:
-			//lightBumper is a macro for all light bump sensors (lightBumpLeft, lightBumpRight, etc)
-			//Unfortunately, no similar macro exists for cliff sensors or bumper switches due to the way the data is delivered.
-			if(chg.lightBumper || chg.bumpLeft || chg.bumpRight || chg.dropLeft || chg.dropRight || chg.clean || chg.docked) {
-				driveLogic(); //Run drive logic only when sensor values change.
-			}
-
-		    //Charging Station Detected! Since it's in front of the robot anyway... Start Auto-Docking!
-		    // if battery is low start docking
-		if(charge <= 500)/*&&(robot.data.irLeft == 172 || robot.data.irRight == 172))*/
-		   {
-				robot.drive(0,0); run = -1; robot.showText("SEEK", 500, false, robot.autoDock);
-			}
-		} else if(robot.data.mode == 1) { //PASSIVE mode:
-			if(chg.clean && robot.data.clean) { //Clean Pressed:
-				if(robot.data.docked) robot.clean(); //Start backing up if clean pressed while docked.
-				else preventDefault(function(){run = 1; driveLogic()}); //Prevent default.
-			}
-			if(chg.docked && robot.data.docked) { //Robot Docked:
-				setUndock(0); robot.full(); robot.showText("DOCK", 500, false, robot.start);
-			} else if(chg.docked/* && !robot.data.charger*/ && !robot.data.docked) { //Robot Undocked:
-				setUndock(1);
-			} else if(chg.dropLeft || chg.dropRight) { //Docking Interrupted:
-				if(run == -1) {robot.full();robot.showText("RST", 500, false, robot.autoDock)}
-				//else setUndock(1);
-			}
-		}
-	}
-
-	//Logic to Start and Stop Moving Robot:
-	function driveLogic() {
-		//We're in user-control (FULL mode) and can control the robot. (Your main program would be here!)
-		if(robot.data.lightBumper || robot.data.bumpLeft || robot.data.bumpRight) robot.driveSpeed(0,0); //Disable motors.
-		else robot.driveSpeed(robot.data.dropLeft?0:0,robot.data.dropRight?0:0); //Enable motors if wheels are up.
-		if(robot.data.clean || robot.data.docked) {robot.driveSpeed(0,0);robot.start()} //Back to PASSIVE mode.
-		// this is where our output data could be.
-		updateData();
-	}
-
-	//Enable and disable undocking timer:
-	var dTmr; function setUndock(e) {
-		if(dTmr) clearTimeout(dTmr); //Cancel timer if already running.
-		if(e) { run = 1; robot.start(); dTmr = setTimeout(function() {
-			robot.full(); run = 1; dTmr = setTimeout(function(){robot.showText
-			("UNDOCK", 250, true);robot.play(0);driveLogic();dTmr=null},250);
-		}, 4400); } else run = 0;
-	}
-
-	//Turns robot when 't' is pressed:
-	var drRun = 0, drAngle = 0;
-	turnRobot = function() {
-		if(robot.data.mode == 3 && drRun) { //If already turning:
-			run = 0; if(drAngle) drAngle = 0; else //Set desired angle to original angle.
-			drAngle = (robot.motorRad==1)?64:-64; //Continue in current motor direction.
-			robot.drive(100, (drAngle-angle<0)?-1:1); drRun = 1;
-		} else if(robot.data.mode == 3 && run == 1) { //Start new turn in opposite direction:
-			run = 0; drAngle = drAngle<0?64:-64; angle = 0; drRun = 1;
-			robot.drive(100, (drAngle-angle<0)?-1:1);
-		}
-	}
-
-
-	var angle = 0; //Count Angle Changes Using Encoders:
-	robot.onMotion = function() {
-		updateData(); // had to place this here to get correct updated data for webmonitor
-		angle += robot.delta.angle; console.log("Angle:", angle);
-		if(((drAngle >= 0 && angle >= drAngle) || (drAngle < 0 && angle
-		<= drAngle)) && drRun) { drRun = 0; run = 1; driveLogic(); }
-	}
-
-	//Prevent Default Behavior of Buttons in Passive Mode:
-	function preventDefault(func) {
-		setTimeout(function(){robot.full();if(func)setTimeout(func,500)},1400);
-	}
-}
-
-function handleInput(robot) {
-	//Process user input, quit on 'exit'
-	const rl = require('readline').createInterface
-	({input:process.stdin, output:process.stdout});
-	rl.on('line', function(text) {
-		if(text == "exit" || text == "quit") {
-			console.log("Exiting..."); process.exit();
-		} else if(text == "t") {
-			turnRobot(); //Turn Robot.
-		} else if(text == "s") {
-			stop(); //Stop Turning.
-		} else if(text == "stop") {
-			stop(); //Turn Robot.
-		} else if(text == "w") {
-			forward(); //Turn Robot.
-		} else if(text == "x") {
-			backward(); //Turn Robot.
-		} else if(text == "a") {
-			turnLeft(); //Turn Robot.
-		} else if(text == "d") {
-			turnRight(); //Turn Robot.
-		} else if(text == "ball") {
-		    action();
-
-//	setTimeout(function(){ballfollow()},i * 1000);
-		} else if(text == "wander") {
-		    setTimeout(function(){robot.driveSpeed(100,100)},0);
-		    setTimeout(function(){robot.driveSpeed(-100,-100)},2500);
-
-		} else if(text == "undock"){
-		    //undock();
-		    mode = 3;
-		} else if(text == "mode1"){
-		    mode = 1;
-		} else if(text == "dock"){
-		    robot.drive(0,0); run = -1; robot.showText("SEEK", 500, false, robot.autoDock);
-		    
-		}
-	    
-	    
-	});
-=======
     robot = r;
     handleInput(robot);
     //Enter Full Mode:
@@ -667,7 +411,6 @@ function handleInput(robot) {
 
         }
     });
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
 }
 
 
@@ -705,31 +448,10 @@ turnLeft = function() {
     robot.driveSpeed(-100, 100);
 }
 
-<<<<<<< HEAD
-undock = function(){
-    console.log("undocking");
-//    robot.full(); var run = 1;    
-    robot.data.mode = 3;
-    run = 1;
-}
-
-
-ballfollow = function(){
-  if(x2 <= 150){
-    robot.driveSpeed(-50,50);
-  }
-  else if(x2 >= 450){
-    robot.driveSpeed(50,-50);
-  }
-  else {
-    robot.driveSpeed(0,0);
-  }
-=======
 stop = function() {
     motorLeft = 0;
     motorRight = 0;
     robot.driveSpeed(0, 0);
->>>>>>> 739e04a507b26ef6c77669f34cb20b71c831eb3b
 }
 
 start();
--- trackingwebcam.py
+++ trackingwebcam.py
@@ -2,17 +2,48 @@
 # coding: utf-8
 
 
+#----------------------------------------------------------------
+# Autor: Saymon C. A. Oliveira
+# Email: saymowan@gmail.com
+# Descrição: este algoritmo descreve a sexta implementação de OpenCV
+# Funções: Imagem digital -> Transformação HSV -> Imagem binária -> Erosão binária -> Encontrar área -> Encontrar coordenadas
+# Tecnologias: OpenCV, Python e NumPy
+#---------------------------------------------------------------
+
+
 import cv2.cv as cv
 import cv2 as cv2
 import time
 import numpy as np
 import sys
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-
 
 #import RPi.GPIO as gpio
 
+
+
+# Faixa de HSV que usamos para detectar o objeto colorido
+# Neste exemplo, pré definidos para uma bola verde
+#Hmin = 42
+#Hmax = 92
+#Smin = 62
+#Smax = 255
+#Vmin = 63
+#Vmax = 235
+
+##
+#158
+##36
+#28
+#
+
+
+#Padrão RED
+#Hmin = 0
+#Hmax = 120
+#Smin = 171
+#Smax = 255
+#Vmin = 126
+#Vmax = 255
 
 #green tape
 Hmin = 40
@@ -22,6 +53,8 @@ Smax = 255
 Vmin = 100
 Vmax = 255
 
+
+
  # Cria-se um array de valores HSV(mínimo e máximo)
 rangeMin = np.array([Hmin, Smin, Vmin], np.uint8)
 rangeMax = np.array([Hmax, Smax, Vmax], np.uint8)
@@ -29,79 +62,49 @@ rangeMax = np.array([Hmax, Smax, Vmax], np.uint8)
 # Área mínima á ser detectada
 minArea = 50
 
+cv.NamedWindow("Entrada")
+cv.NamedWindow("HSV")
+cv.NamedWindow("Thre")
+cv.NamedWindow("Erosao")
+
 def main():
-#    capture = cv2.VideoCapture(0)
-#    capture = PiCamera()
+    capture = cv2.VideoCapture(0)
+
     # Parametros do tamannho da imagem de captura
-    camera = PiCamera()
-    camera.resolution = (320, 240)
-    camera.framerate = 10
-    rawCapture = PiRGBArray(camera, size=(320, 240))
+    width = 640
+    height = 480
 
-    time.sleep(0.1)
+    # Definir um tamanho para os frames (descartando o PyramidDown
+    if capture.isOpened():
+      capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+      capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
 
-    # capture frames from the camera
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image, then initialize the timestamp
-        # and occupied/unoccupied text
-        image = frame.array
-        imgHSV = cv2.cvtColor(image,cv2.cv.CV_BGR2HSV)
+
+    while True:
+        ret, entrada = capture.read()
+        imgHSV = cv2.cvtColor(entrada,cv2.cv.CV_BGR2HSV)
         imgThresh = cv2.inRange(imgHSV, rangeMin, rangeMax)
         imgErode = cv2.erode(imgThresh, None, iterations = 3)
         moments = cv2.moments(imgErode, True)
         if moments['m00'] >= minArea:
-            x = moments['m10'] / moments['m00']
-            y = moments['m01'] / moments['m00']
-            #print(x, ", ", y)
-            #sets size of cirlce.. can use this to determine stuff.
-            cv2.circle(image, (int(x), int(y)), 25, (0, 255, 0), -1)
-            #print("x: ", x, "y: ", y)
-            print (x, y)
-            sys.stdout.flush()
-                                                                                                                
-        # show the frame
-        cv2.imshow("Frame", image)
+           x = moments['m10'] / moments['m00']
+           y = moments['m01'] / moments['m00']
+           #print(x, ", ", y)
+           #sets size of cirlce.. can use this to determine stuff.
+           cv2.circle(entrada, (int(x), int(y)), 50, (0, 255, 0), -1)
+           #lines_sum = x
+           #print("x: ", x, "y: ", y)
+           print (x, y)
+           sys.stdout.flush()
+
+        cv2.imshow("Entrada",entrada)
         cv2.imshow("HSV", imgHSV)
         cv2.imshow("Thre", imgThresh)
         cv2.imshow("Erosao", imgErode)
-        key = cv2.waitKey(1) & 0xFF
 
-        
-        # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
-    
-    
-    # Definir um tamanho para os frames (descartando o PyramidDown
-#    if capture.isOpened():
-#      capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
-#      capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
-
-
-#    while True:
-#        ret, entrada = capture.read()
-#        imgHSV = cv2.cvtColor(entrada,cv2.cv.CV_BGR2HSV)
-#        imgThresh = cv2.inRange(imgHSV, rangeMin, rangeMax)
-#        imgErode = cv2.erode(imgThresh, None, iterations = 3)
-#        moments = cv2.moments(imgErode, True)
-#        if moments['m00'] >= minArea:
-#           x = moments['m10'] / moments['m00']
-#           y = moments['m01'] / moments['m00']
-           #print(x, ", ", y)
-           #sets size of cirlce.. can use this to determine stuff.
-#           cv2.circle(entrada, (int(x), int(y)), 50, (0, 255, 0), -1)
-           #lines_sum = x
-           #print("x: ", x, "y: ", y)
-#           print (x, y)
-#           sys.stdout.flush()
-
-#        cv2.imshow("Entrada",entrada)
-#        cv2.imshow("HSV", imgHSV)
-#        cv2.imshow("Thre", imgThresh)
-#        cv2.imshow("Erosao", imgErode)
-
-#        if cv.WaitKey(10) == 27:
-#            break
-#    cv.DestroyAllWindows()
+        if cv.WaitKey(10) == 27:
+            break
+    cv.DestroyAllWindows()
 
 
 if __name__ == '__main__':
